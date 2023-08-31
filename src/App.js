69628030830d0a80779e15ec4d3316782c8e6ecd
dtienhau <117889242+dtienhau@users.noreply.github.com>
2023-08-31 13:52:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = 'https://nailsforus.com' + window.location.pathname;
+    window.location.href = 'https://realmamazing.com' + window.location.pathname;
   }, []);
 
   return null;
